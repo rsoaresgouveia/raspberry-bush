4850a5ee6791814269441a94064ee29197d5f20c
Rodrigo Gouveia <rsoares.developer@gmail.com>
2021-08-11 23:38:49 -0400

    :shinto_shrine: making architetural changes in my .sh files

--- cl/raspberry-bush-start.sh
+++ cl/raspberry-bush-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/program
+cd ~/bin
 sudo ./raspberry-bush  
 echo "all done! hit enter to close "
 read foo
--- startserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh pi@192.168.0.82 "cd program && ./raspberry-bush"
